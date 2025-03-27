170ed8cbac5b299c06e0f0d55c891c5bddc93d23
henrique <luiz.isbaes@feltracoepertusatti.com.br>
2025-03-27 11:59:39 -0300

    tirando jwt para todas as rotas api, apenas para as rotas com o middleware jwt.auth

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,7 @@ use App\Http\Middleware\JwtAuthMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
+        web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
@@ -14,10 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'jwt.auth' => JwtAuthMiddleware::class,
-        ]);
-
-        $middleware->api(prepend: [
-            'jwt.auth',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
